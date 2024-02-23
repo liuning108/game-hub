f5fe3069ae5454fd02a1cbf7982d3b5ce09c1671
Liu ning <122273014@qq.com>
2024-02-24 05:31:07 +0800

    add a  function  comment

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,23 @@
 import { Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGame";
-
-
+/**
+ * 这个函数渲染游戏列表，并在获取游戏数据时处理任何错误。
+ *
+ * @return {JSX.Element} 游戏列表或错误消息。
+ * 
+ */
 function GameGrid() {
-  const {games,error} = useGames()
+  const { games, error } = useGames();
   return (
-      <>
+    <>
       {error && <Text>{error}</Text>}
       <ul>
-        {games.map(g=><li key={g.id}>{g.name}</li>)}
+        {games.map((g) => (
+          <li key={g.id}>{g.name}</li>
+        ))}
       </ul>
-      </>
-  )
+    </>
+  );
 }
 
 export default GameGrid;
- 
