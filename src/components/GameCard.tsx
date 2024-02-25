6505a68adb016aa69bf4f7c6e739fcff041146cb
Liu ning <122273014@qq.com>
2024-02-25 16:12:12 +0800

    add black theme

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from '../services/image-url';
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -13,7 +14,7 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize={"x"}>{game.name}</Heading>
+        <Heading fontSize={"x"}>{game.name}<Emoji rating={game.rating_top}/></Heading>
         <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
